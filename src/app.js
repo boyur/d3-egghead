8f743b57fd2573cc1024952e39defc0c2ad8d309
Artem Boyur <gboyur@gmail.com>
2017-03-17 16:17:03 +0300

    lesson 1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = `<h1>${d3.version}</h1>`;
+const data = [5, 10, 15, 20, 25];
+
+d3.select('body').selectAll('div')
+  .data(data)
+  .enter()
+  .append('div')
+  .attr('class', 'bar')
+  .style('height', function (d) {
+    return d * 5 + 'px'
+  });
+
